f43d715fc472f1f2a0b6151807e832d31389800a
jynam17 <73823388+jynam17@users.noreply.github.com>
2020-11-05 20:36:27 +0900

    소스 전체 첫 업로드

---
+++ JJS_NamJuyeong/java/Top.java
@@ -0,0 +1,17 @@
+package com.example.closet;
+
+public class Top {
+    Integer topID;
+
+    public Top(Integer topID) {
+        this.topID = topID;
+    }
+
+    public Integer getTopID() {
+        return topID;
+    }
+
+    public void setTopID(Integer topID) {
+        this.topID = topID;
+    }
+}
